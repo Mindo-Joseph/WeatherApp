e29217823e865e6b5ac151c84f31126d78268cd5
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-10-15 15:42:27 +0300

    Add function to convert farenheight to celcius

--- src/apirequest.js
+++ src/apirequest.js
@@ -1,9 +1,10 @@
 const openWeatherApi = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
 async function getWeather(cityname) {
-  const response = await fetch(`${openWeatherApi}${cityname}&appid=${process.env.API_KEY}`);
+  const response = await fetch(`${openWeatherApi}${cityname}&units=imperial&appid=${process.env.API_KEY}`);
   const weatherData = await response.json();
   return weatherData;
 }
+export const fToC = (farenheight) => Math.floor((farenheight - 32) * (5 / 9));
 
 export default getWeather;
